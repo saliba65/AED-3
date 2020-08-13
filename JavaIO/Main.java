87ea642b195ba8c0a9a9d2f1faec439d5aac0070
saliba65 <lucassaliba10@gmail.com>
2020-08-13 09:33:44 -0300

    Random AcessFile

--- JavaIO/Main.java
+++ JavaIO/Main.java
@@ -1,7 +1,5 @@
-import java.io.DataOutputStream;
-import java.io.FileOutputStream;
-import java.io.DataInputStream;
-import java.io.FileInputStream;
+
+import java.io.RandomAccessFile;
 
 public class Main {
 
@@ -9,52 +7,48 @@ public class Main {
 
     Livro l1 = new Livro(1, "Naruto", "Masashi Kishimoto", 14.9F);
     Livro l2 = new Livro(2, "Dragon Ball", "Akira Toriyama", 21.99F);
-    Livro l3 = new Livro(3, "One Piece", "Eiichiro Oda", 17.86F);
 
-    FileOutputStream arq;
-    DataOutputStream dos;
-
-    FileInputStream arq2;
-    DataInputStream dis;
-
+    RandomAccessFile arq;
     byte[] ba;
 
     try {
 
       // * ESCRITA *
-      arq = new FileOutputStream("dados/livros.db");
-      dos = new DataOutputStream(arq);
+      arq = new RandomAccessFile("dados/livros.db", "rw");
+
+      long p1 = arq.getFilePointer();
       ba = l1.toByteArray();
-      dos.writeInt(ba.length);
+      arq.writeInt(ba.length);
       arq.write(ba);
 
+      long p2 = arq.getFilePointer();
       ba = l2.toByteArray();
-      dos.writeInt(ba.length);
+      arq.writeInt(ba.length);
       arq.write(ba);
-
-      arq.close();
 
       // * LEITURA *
 
+      Livro l3 = new Livro();
       Livro l4 = new Livro();
       // Livro l5 = new Livro();
       int tam;
 
-      arq2 = new FileInputStream("dados/livros.db");
-      dis = new DataInputStream(arq2);
-
-      tam = dis.readInt();
+      arq.seek(p2);
+      tam = arq.readInt();
       ba = new byte[tam];
-      dis.read(ba);
+      arq.read(ba);
       l3.fromByteArray(ba);
 
-      tam = dis.readInt();
+      arq.seek(p1);
+      tam = arq.readInt();
       ba = new byte[tam];
-      dis.read(ba);
+      arq.read(ba);
       l4.fromByteArray(ba);
 
       System.out.println(l3);
       System.out.println(l4);
+
+      arq.close();
 
     } catch (Exception e) {
       e.printStackTrace();
